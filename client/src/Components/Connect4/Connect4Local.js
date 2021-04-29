5a80771bba25cbed9a71eb603e38ec36b35759d2
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2021-04-29 17:57:34 -0400

    Completed local Connect4

--- client/src/Components/Connect4/Connect4Local.js
+++ client/src/Components/Connect4/Connect4Local.js
@@ -1,6 +1,67 @@
 import React, { useState } from 'react';
 import './Connect4.css'
 
+// constants
+const HEIGHT = 6;
+const WIDTH = 7;
+const LIMIT = 3;
+
+// function to check the length of the column given
+function columnLength(column) {
+    var count = 0;
+    while (column[count]) {
+        count++;
+    }
+    return count;
+}
+
+// function to check if there is a winner
+function winnerCheck(columns) {
+    // horizontal check
+    for (let i = 0; i < HEIGHT; i++) {
+        for (let j = 0; j < WIDTH - LIMIT; j++) {
+            if (columns[j][i] && columns[j][i] === columns[j + 1][i] &&
+                columns[j][i] === columns[j + 2][i] && columns[j][i] === columns[j + 3][i]) {
+                    return columns[j][i];
+            }
+        }
+    }
+
+    // vertical check
+    for (let i = 0; i < HEIGHT - LIMIT; i++) {
+        for (let j = 0; j < WIDTH; j++) {
+            if (columns[j][i] && columns[j][i] === columns[j][i + 1] &&
+                columns[j][i] === columns[j][i + 2] && columns[j][i] === columns[j][i + 3]) {
+                    return columns[j][i];
+            }
+        }
+    }
+
+    // top-right diagonal check
+    for (let i = 0; i < HEIGHT - LIMIT; i++) {
+        for (let j = 0; j < WIDTH - LIMIT; j++) {
+            if (columns[j][i] && columns[j][i] === columns[j + 1][i + 1] &&
+                columns[j][i] === columns[j + 2][i + 2] && columns[j][i] === columns[j + 3][i + 3]) {
+                    return columns[j][i];
+            }
+        }
+    }
+
+    // bottom-right diagonal check
+    for (let i = LIMIT; i < HEIGHT; i++) {
+        for (let j = 0; j < WIDTH - LIMIT; j++) {
+            if (columns[j][i] && columns[j][i] === columns[j + 1][i - 1] &&
+                columns[j][i] === columns[j + 2][i - 2] && columns[j][i] === columns[j + 3][i - 3]) {
+                    return columns[j][i];
+            }
+        }
+    }
+
+    // no winner
+    return false;
+}
+
+// individual square
 const Square = (props) => {
     return (
         <div className="square">
@@ -9,73 +70,113 @@ const Square = (props) => {
     );
 }
 
-const Board = () => {
-    const [column, setColumn] = useState([[], [], [], [], [], [], []]);
-    const [color, setColor] = useState('red');
-
-    const square = (i) => {
-        return <Square color={i} />;
-    }
-
-    const handleClick = (i) => {
-        var newColumn = column.slice();
-        if (newColumn[i].length >= 6) {
-            return;
-        }
-        newColumn[i].push(color);
-        if (color === 'red') {
-            setColor('yellow');
-        } else {
-            setColor('red');
-        }
-        setColumn(newColumn);
-    }
-
-    const columns = (i) => {
-        var squares = i.slice().reverse();
-        var len = squares.length;
-        var output = [];
-        for (let k = 0; k < 6 - len; k++) {
-            output.push(square('#bbb'));
-        }
-        for (let k = 6 - len; k < 6; k++) {
-            output.push(square(squares[k - (6 - len)]));
-        }
-        return output;
-    }
-
+// game board
+const Board = (props) => {
     return (
-        <div className="boardConnect4">
-            <button className="columns" onClick={() => handleClick(0)}>
-                {columns(column[0])}
+        <div >
+            <button className="columns" onClick={() => props.handleClick(0)}>
+                {props.columns(0)}
             </button>
-            <button className="columns" onClick={() => handleClick(1)}>
-                {columns(column[1])}
+            <button className="columns" onClick={() => props.handleClick(1)}>
+                {props.columns(1)}
             </button>
-            <button className="columns" onClick={() => handleClick(2)}>
-                {columns(column[2])}
+            <button className="columns" onClick={() => props.handleClick(2)}>
+                {props.columns(2)}
             </button>
-            <button className="columns" onClick={() => handleClick(3)}>
-                {columns(column[3])}
+            <button className="columns" onClick={() => props.handleClick(3)}>
+                {props.columns(3)}
             </button>
-            <button className="columns" onClick={() => handleClick(4)}>
-                {columns(column[4])}
+            <button className="columns" onClick={() => props.handleClick(4)}>
+                {props.columns(4)}
             </button>
-            <button className="columns" onClick={() => handleClick(5)}>
-                {columns(column[5])}
+            <button className="columns" onClick={() => props.handleClick(5)}>
+                {props.columns(5)}
             </button>
-            <button className="columns" onClick={() => handleClick(6)}>
-                {columns(column[6])}
+            <button className="columns" onClick={() => props.handleClick(6)}>
+                {props.columns(6)}
             </button>
         </div>
     );
 }
 
 const Connect4Local = () => {
+    const [column, setColumn] = useState([Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null)]);
+    const [color, setColor] = useState('red');
+    const [winCount, setWinCount] = useState([0, 0]);
+
+    // creating a square based on given color
+    const square = (i) => {
+        return <Square color={i} />;
+    }
+
+    // creating column number i
+    const columns = (i) => {
+        var squares = column[i].slice();
+        var len = columnLength(squares);
+        squares.reverse();
+        var output = Array(6);
+        var k = 0;
+        for ( ; k < 6 - len; k++) {
+            output[k] = square('#bbb');
+        }
+        for ( ; k < 6; k++) {
+            output[k] = square(squares[k]);
+        }
+        return output;
+    }
+
+    // when column clicked
+    const handleClick = (i) => {
+        var newColumn = column.slice();
+        var len = columnLength(newColumn[i]);
+
+        // if there is winner or length is 6
+        if (winnerCheck(column) || len >= 6) {
+            return;
+        }
+
+        // set and change color
+        newColumn[i][len] = color;
+        if (color === 'red') {
+            setColor('yellow');
+        } else {
+            setColor('red');
+        }
+        setColumn(newColumn);
+
+        // checking if there is winner
+        if (winnerCheck(column) === 'red') {
+            setWinCount([winCount[0] + 1, winCount[1]])
+        } else if (winnerCheck(column) === 'yellow') {
+            setWinCount([winCount[0], winCount[1] + 1])
+        }
+    }
+
+    // resetting board
+    const reset = () => {
+        setColor('red');
+        setColumn([Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null), Array(6).fill(null)]);
+    }
+
+    var statusCheck;
+    if (winnerCheck(column)) {
+        statusCheck = 'Winner is: ' + winnerCheck(column);
+    } else {
+        statusCheck = 'Turn: ' + color;
+    }
+
     return (
         <div>
             <div className="title">Connect 4</div>
-            <Board />
+            <Board handleClick={(i) => handleClick(i)} columns={(i) => columns(i)} />
+            <div className="sidebarConnect4">
+                <div className="turnConnect4">
+                    {statusCheck}
+                    <br /><br />
+                    {"Red wins: " + winCount[0] + ", Yellow wins: " + winCount[1]}
+                </div>
+                <button className="buttonConnect4" onClick={() => reset()}>Reset Game</button>
+            </div>
         </div>
     );
 }
